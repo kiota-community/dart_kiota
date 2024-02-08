a5d32af569464252bf8bd2413274fa55a87faa6a
Ricardo Boss <mail@ricardoboss.de>
2024-02-08 13:57:50 +0100

    More API docs

--- lib/src/api_exception.dart
+++ lib/src/api_exception.dart
@@ -1,14 +1,22 @@
 part of '../dart_kiota.dart';
 
+/// Parent type for exceptions thrown by the client when receiving failed
+/// responses to its requests.
 class ApiException implements Exception {
+  /// Creates an instance of [ApiException] with the given parameters.
   ApiException({
     this.statusCode,
     this.message,
     this.responseHeaders,
   });
 
+  /// The HTTP status code of the response.
   final int? statusCode;
+
+  /// An optional message that describes the error.
   final String? message;
+
+  /// The headers of the response.
   final Map<String, List<String>>? responseHeaders;
 
   @override
