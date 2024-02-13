a6c5bb56ebfffd2b4747e4d7f827ba0370a75950
Ricardo Boss <mail@ricardoboss.de>
2024-02-14 00:12:43 +0100

    Formatting

--- test/time_only_test.dart
+++ test/time_only_test.dart
@@ -12,13 +12,16 @@ void main() {
         TimeOnly.fromDateTimeString('12:34').toRfc3339String(),
         '12:34:00',
       );
-      expect(TimeOnly.fromDateTimeString('12').toRfc3339String(), '12:00:00');
+      expect(
+        TimeOnly.fromDateTimeString('12').toRfc3339String(),
+        '12:00:00',
+      );
     });
 
     test('fromDateTime', () {
+      final dateTime = DateTime(2021, 1, 1, 12, 34, 56);
       expect(
-        TimeOnly.fromDateTime(DateTime(2024, 1, 1, 12, 34, 56))
-            .toRfc3339String(),
+        TimeOnly.fromDateTime(dateTime).toRfc3339String(),
         '12:34:56',
       );
     });
@@ -28,8 +31,14 @@ void main() {
         TimeOnly.fromComponents(12, 34, 56, 789).toRfc3339String(),
         '12:34:56.789',
       );
-      expect(TimeOnly.fromComponents(12, 34, 56).toRfc3339String(), '12:34:56');
-      expect(TimeOnly.fromComponents(12, 34).toRfc3339String(), '12:34:00');
+      expect(
+        TimeOnly.fromComponents(12, 34, 56).toRfc3339String(),
+        '12:34:56',
+      );
+      expect(
+        TimeOnly.fromComponents(12, 34).toRfc3339String(),
+        '12:34:00',
+      );
     });
   });
 }
