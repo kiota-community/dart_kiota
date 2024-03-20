5ff6532bd0e4107055e5a3db914ce531c149d18f
Ricardo Boss <mail@ricardoboss.de>
2024-03-20 22:28:14 +0100

    Made backing store factory singleton writable

--- lib/src/store/backing_store_factory_singleton.dart
+++ lib/src/store/backing_store_factory_singleton.dart
@@ -2,8 +2,6 @@ part of '../../kiota_abstractions.dart';
 
 /// This class is used to register the backing store factory.
 class BackingStoreFactorySingleton {
-  static final BackingStoreFactory _instance = InMemoryBackingStoreFactory();
-
   /// The backing store factory singleton instance.
-  static BackingStoreFactory get instance => _instance;
+  static BackingStoreFactory instance = InMemoryBackingStoreFactory();
 }
