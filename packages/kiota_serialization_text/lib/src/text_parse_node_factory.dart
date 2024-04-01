16bc55ee9322ee45c586faa922cc59f44909d23e
Ricardo Boss <mail@ricardoboss.de>
2024-04-01 14:22:18 +0200

    Added text parse node factory

---
+++ packages/kiota_serialization_text/lib/src/text_parse_node_factory.dart
@@ -0,0 +1,17 @@
+part of '../kiota_serialization_text.dart';
+
+class TextParseNodeFactory implements ParseNodeFactory {
+  @override
+  ParseNode getRootParseNode(String contentType, Uint8List content) {
+    if (contentType != validContentType) {
+      throw ArgumentError('Invalid content type');
+    }
+
+    final text = utf8.decode(content);
+
+    return TextParseNode(text);
+  }
+
+  @override
+  String get validContentType => 'text/plain';
+}
