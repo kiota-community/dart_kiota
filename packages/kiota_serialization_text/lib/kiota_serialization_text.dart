03dd4411b157129893b0312d6b9bf8955906ea48
Ricardo Boss <mail@ricardoboss.de>
2024-04-01 12:57:48 +0200

    New Dart project

---
+++ packages/kiota_serialization_text/lib/kiota_serialization_text.dart
@@ -0,0 +1,5 @@
+/// This library implements deserialization for text/plain responses.
+///
+/// This library is not meant to be used directly, but rather to be used as a
+/// dependency in the generated code.
+library kiota_serialization_text;
