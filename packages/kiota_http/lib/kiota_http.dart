c2ccfaa3f9e2e6fb1f74d5f41f4169e507ded4f3
Ricardo Boss <mail@ricardoboss.de>
2024-04-07 15:27:40 +0200

    Use official client wrappers and only provide a custom user agent client

--- packages/kiota_http/lib/kiota_http.dart
+++ packages/kiota_http/lib/kiota_http.dart
@@ -2,17 +2,13 @@
 /// [Kiota](https://github.com/microsoft/kiota) clients.
 library kiota_http;
 
-import 'dart:math';
-import 'dart:mirrors';
 import 'dart:typed_data';
 
 import 'package:http/http.dart' as http;
+import 'package:http/retry.dart' as retry;
 import 'package:kiota_abstractions/kiota_abstractions.dart';
 import 'package:uuid/uuid.dart';
 
 part 'src/http_client_request_adapter.dart';
 part 'src/kiota_client_factory.dart';
-part 'src/middleware/redirect_handler.dart';
-part 'src/middleware/redirect_handler_option.dart';
-part 'src/middleware/retry_handler_option.dart';
-part 'src/middleware/retry_handler.dart';
+part 'src/middleware/user_agent_client.dart';
--- packages/kiota_http/lib/src/kiota_client_factory.dart
+++ packages/kiota_http/lib/src/kiota_client_factory.dart
@@ -4,8 +4,24 @@ class KiotaClientFactory {
   KiotaClientFactory._();
 
   static http.Client createClient() {
-    return RetryHandler(
-      http.Client(),
+    return UserAgentClient(
+      retry.RetryClient(
+        http.Client(),
+        when: (response) {
+          const retryCodes = {
+            408, // Request Timeout
+            429, // Too Many Requests
+            500, // Internal Server Error
+            502, // Bad Gateway
+            503, // Service Unavailable
+            504, // Gateway Timeout
+          };
+
+          return retryCodes.contains(response.statusCode);
+        },
+      ),
+      // TODO(ricardoboss): Get the version from the pubspec
+      userAgent: 'kiota_http/0.0.1',
     );
   }
 }
