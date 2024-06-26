43ffa769ed02acc9855e119d771d464525bc128b
Ricardo Boss <mail@ricardoboss.de>
2024-06-26 20:30:38 +0200

    Allow serialization writer methods to return Futures

--- packages/kiota_abstractions/lib/src/serialization/serialization_writer.dart
+++ packages/kiota_abstractions/lib/src/serialization/serialization_writer.dart
@@ -4,51 +4,54 @@ part of '../../kiota_abstractions.dart';
 abstract class SerializationWriter {
   /// Writes the specified string value to the buffer with an optional given
   /// [key].
-  void writeStringValue(String? key, String? value);
+  FutureOr<void> writeStringValue(String? key, String? value);
 
   /// Writes the specified int value to the buffer with an optional given
   /// [key].
-  void writeBoolValue(String? key, {bool? value});
+  FutureOr<void> writeBoolValue(String? key, {bool? value});
 
   /// Writes the specified int value to the buffer with an optional given
   /// [key].
-  void writeIntValue(String? key, int? value);
+  FutureOr<void> writeIntValue(String? key, int? value);
 
   /// Writes the specified double value to the buffer with an optional given
   /// [key].
-  void writeDoubleValue(String? key, double? value);
+  FutureOr<void> writeDoubleValue(String? key, double? value);
 
   /// Writes the specified [DateTime] value to the buffer with an optional given
   /// [key].
-  void writeDateTimeValue(String? key, DateTime? value);
+  FutureOr<void> writeDateTimeValue(String? key, DateTime? value);
 
   /// Writes the specified collection of primitive values to the buffer with an
   /// optional given [key].
-  void writeCollectionOfPrimitiveValues<T>(String? key, Iterable<T>? values);
+  FutureOr<void> writeCollectionOfPrimitiveValues<T>(
+    String? key,
+    Iterable<T>? values,
+  );
 
   /// Writes the specified collection of [T] object values to the buffer with an
   /// optional given [key].
-  void writeCollectionOfObjectValues<T extends Parsable>(
+  FutureOr<void> writeCollectionOfObjectValues<T extends Parsable>(
     String? key,
     Iterable<T>? values,
   );
 
   /// Writes the specified collection of enum [T] values to the buffer with an
   /// optional given [key].
-  void writeCollectionOfEnumValues<T extends Enum>(
+  FutureOr<void> writeCollectionOfEnumValues<T extends Enum>(
     String? key,
     Iterable<T>? values,
   );
 
   /// Writes the specified byte list as a base64 string to the buffer with an
   /// optional given [key].
-  void writeByteArrayValue(String? key, Uint8List? value);
+  FutureOr<void> writeByteArrayValue(String? key, Uint8List? value);
 
   /// Writes the specified model object to the buffer with an optional given
   /// [key].
   /// The [additionalValuesToMerge] parameter is used to merge additional
   /// values to the main object when serializing an intersection wrapper.
-  void writeObjectValue<T extends Parsable>(
+  FutureOr<void> writeObjectValue<T extends Parsable>(
     String? key,
     T? value, [
     Iterable<Parsable>? additionalValuesToMerge,
@@ -56,16 +59,16 @@ abstract class SerializationWriter {
 
   /// Writes the specified enum value to the buffer with an optional given
   /// [key].
-  void writeEnumValue<T extends Enum>(String? key, T? value);
+  FutureOr<void> writeEnumValue<T extends Enum>(String? key, T? value);
 
   /// Writes a null value for the specified [key].
-  void writeNullValue(String? key);
+  FutureOr<void> writeNullValue(String? key);
 
   /// Writes the specified additional data to the buffer.
-  void writeAdditionalData(Map<String, dynamic> value);
+  FutureOr<void> writeAdditionalData(Map<String, dynamic> value);
 
   /// Gets the value of the serialized content.
-  Uint8List getSerializedContent();
+  FutureOr<Uint8List> getSerializedContent();
 
   /// Callback called before the serialization process starts.
   ParsableHook? onBeforeObjectSerialization;
