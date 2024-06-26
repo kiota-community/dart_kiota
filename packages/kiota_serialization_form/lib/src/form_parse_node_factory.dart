98a59488f01dca5f578bfd04f0ed778137d6e960
Ricardo Boss <mail@ricardoboss.de>
2024-06-26 10:07:00 +0200

    Added FormParseNodeFactory

---
+++ packages/kiota_serialization_form/lib/src/form_parse_node_factory.dart
@@ -0,0 +1,17 @@
+part of '../kiota_serialization_form.dart';
+
+class FormParseNodeFactory implements ParseNodeFactory {
+  @override
+  ParseNode getRootParseNode(String contentType, Uint8List content) {
+    if (contentType != validContentType) {
+      throw ArgumentError('Invalid content type');
+    }
+
+    final text = utf8.decode(content);
+
+    return FormParseNode(text);
+  }
+
+  @override
+  String get validContentType => 'application/x-www-form-urlencoded';
+}
