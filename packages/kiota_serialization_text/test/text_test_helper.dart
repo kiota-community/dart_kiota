1308be146630892768cfd160a1742ae9466e134a
Ricardo Boss <mail@ricardoboss.de>
2024-07-18 03:17:41 +0200

    Formatting

--- packages/kiota_serialization_text/test/text_test_helper.dart
+++ packages/kiota_serialization_text/test/text_test_helper.dart
@@ -2,7 +2,7 @@ import 'package:test/test.dart';
 
 final throwsNoStructuredDataError = throwsA(
   isA<UnsupportedError>().having(
-        (e) => e.message,
+    (e) => e.message,
     'message',
     equals('Text does not support structured data'),
   ),
