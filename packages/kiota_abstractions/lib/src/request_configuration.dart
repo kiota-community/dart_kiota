ac2c00bdae2207befe6d57c1976a5db72b93c055
Joanne ter Maat <joanne.ter.maat@topicus.nl>
2024-10-09 07:48:28 +0200

    don't use late

--- packages/kiota_abstractions/lib/src/request_configuration.dart
+++ packages/kiota_abstractions/lib/src/request_configuration.dart
@@ -2,11 +2,13 @@ part of '../kiota_abstractions.dart';
 
 /// Request configuration type for [BaseRequestBuilder]s.
 class RequestConfiguration<T extends AbstractQueryParameters> {
+
+  RequestConfiguration({required this.queryParameters});
   /// The HTTP headers of the request.
   HttpHeaders headers = HttpHeaders();
 
   /// The request options.
   List<RequestOption> options = [];
 
-  late T queryParameters;
+  T queryParameters;
 }
